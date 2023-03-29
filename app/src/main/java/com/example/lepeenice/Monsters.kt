3785dc7661fefbdcba625595b8b9a7addddf1d19
Hellmesss <gdemonet@yahoo.fr>
2023-03-29 17:21:49 -0400

    Add - Empty class of monsters + first monster

---
+++ app/src/main/java/com/example/lepeenice/Monsters.kt
@@ -0,0 +1,25 @@
+package com.example.lepeenice
+
+class Monsters constructor(var name : String,var maxLife : Int,var sprite : Int){
+
+    // verifier si le mob est mort
+
+    // verifier à combien de % est la vie de mob pour changer son sprite
+
+        /*fun updateSprite(newSprite: Int)
+        {
+            if ( currentLife <  currentLife/2)
+            {
+                sprite = newSprite
+            }
+        }*/
+
+    // faire spawn un nouveau mob
+
+    // initialiser le du mob
+
+    val monster1 = Monsters("Goblin", 100, R.drawable.mob1)
+
+    //monster1.updateSprite(R.Drawable.mob1_evolved)
+
+}
