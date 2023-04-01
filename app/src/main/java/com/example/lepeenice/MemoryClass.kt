4555565ff83042a2b70d229e4a89e807df04c958
Julien PIRAT <72934794+JulienPIRAT@users.noreply.github.com>
2023-04-01 15:44:53 -0400

    [FEAT] - Save Working for player

--- app/src/main/java/com/example/lepeenice/MemoryClass.kt
+++ app/src/main/java/com/example/lepeenice/MemoryClass.kt
@@ -10,12 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lepeenice.MemoryClassPackage.GameManager
 import com.example.lepeenice.MemoryClassPackage.Player
+import com.example.lepeenice.MemoryClassPackage.SaveManager
 import com.example.lepeenice.ui.theme.LEpeeNiceTheme
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.decodeFromString
@@ -36,32 +38,18 @@ class MemoryClass {
                         Text(text = "Version : 0.0.1")
                         Spacer(modifier = Modifier.height(16.dp))
 
+                        SaveManager.getInstance().loadDataFromSharedPreferences(LocalContext.current)
+
                         val Player = Player.getInstance()
-                        Player.pseudo = "John"
                         Text(text = "Pseudo : "+Player.pseudo)
-                        Text(text = "Pseudo : "+Player.pseudo)
-                        //Text(text = "Score : "+p.score.toString())
 
-                        val GameManager = GameManager.getInstance()
-                        GameManager.createSwords()
-
-                        GameManager.addMonster("Gobelin",10,1,0,R.drawable.mob1,R.drawable.mob1_evolved, 1,-1)
-
-
-
-                        //Text(text = "InitGameManager : "+GameManager.printMonsters())
 
                         //SERIALIZATION
-                        val EncodedJson = Json.encodeToString(Player)
-                        Text(text = "SerializedFiled : "+EncodedJson)
+                        //val EncodedJson = Json.encodeToString(Player)
+                        //SaveManager.getInstance().saveDataToSharedPreferences_Player(LocalContext.current, EncodedJson)
 
                         Spacer(modifier = Modifier.height(16.dp))
 
-                        //DESERIALIZATION
-                        val DecodedJson = Json.decodeFromString<Player>(EncodedJson)
-                        Text(text = "DeserializedFiled : "+DecodedJson)
-
-                        Spacer(modifier = Modifier.height(16.dp))
 
                         // Zone d'entrée de texte
                         val textState = remember { mutableStateOf(TextFieldValue()) }
---
+++ app/src/main/java/com/example/lepeenice/MemoryClassPackage/SaveManager.kt
@@ -0,0 +1,53 @@
+package com.example.lepeenice.MemoryClassPackage
+
+import android.content.Context
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+public class SaveManager private constructor(){
+
+    private val PlayerKeySaved: String = "LepeeniceSave_Player"
+    private val GameManagerKeySaved: String = "LepeeniceSave_GameManager"
+
+    companion object {
+        private var instance: SaveManager? = null
+
+        fun getInstance(): SaveManager {
+            if (instance == null) {
+                instance = SaveManager()
+            }
+            return instance as SaveManager
+        }
+    }
+
+    fun saveDataToSharedPreferences_Player(context: Context, data: String) {
+        val sharedPreferences = context.getSharedPreferences(PlayerKeySaved, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString(PlayerKeySaved, data).apply()
+    }
+
+    fun saveDataToSharedPreferences_GameManager(context: Context, data: String) {
+        val sharedPreferences = context.getSharedPreferences(GameManagerKeySaved, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString(GameManagerKeySaved, data).apply()
+    }
+
+    fun loadDataFromSharedPreferences(context: Context) {
+        val sharedPreferences_Player = context.getSharedPreferences(PlayerKeySaved, Context.MODE_PRIVATE)
+        val sharedPreferences_GameManager = context.getSharedPreferences(GameManagerKeySaved, Context.MODE_PRIVATE)
+        val resString_Player = sharedPreferences_Player.getString(PlayerKeySaved, "") ?: ""
+        //val resString_GameManager = sharedPreferences_GameManager.getString(GameManagerKeySaved, "") ?: ""
+
+        if(resString_Player != ""){
+            val DecodedJson_Player = Json.decodeFromString<Player>(resString_Player)
+            Player.getInstance().loadSave(DecodedJson_Player)
+        }
+/*
+        if(resString_GameManager != ""){
+            val DecodedJson_GameManager = Json.decodeFromString<GameManager>(resString_GameManager)
+            Player.getInstance().loadSave(DecodedJson_Player)
+        }
+         */
+
+    }
+
+
+}
